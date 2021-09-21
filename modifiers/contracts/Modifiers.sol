b7ac6a89d9752c0870095a478f3a9292b964265d
Cypher <cypher.build@protonmail.com>
2021-09-21 00:21:52 -0500

    update.

--- modifiers/contracts/Modifiers.sol
+++ modifiers/contracts/Modifiers.sol
@@ -4,14 +4,15 @@ pragma solidity ^0.8.0;
 contract Modifiers {
     uint public minimumBid = 1000;
 
-    modifier min(uint amount) {
-        if(amount > minimumBid){
+    modifier min() {
+        if(msg.value >= minimumBid){
+            _;
+        } else {
             revert("Minimum bid threshold not met.");
         }
-        _;
     }
 
-    function bid(uint amount) external min(amount) {
-
+    function bid(uint amount) external payable min returns(bool) {
+        return true;
     }
 }
